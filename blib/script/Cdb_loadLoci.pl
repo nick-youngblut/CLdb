61bf64faf2a3657ae790db911548ccab6ccae409
Nick Youngblut <nyoungb2@illinois.edu>
2013-06-27 15:32:36 -0500

    build commit

---
+++ blib/script/Cdb_loadLoci.pl
@@ -0,0 +1,208 @@
+#!/usr/bin/env perl
+
+### modules
+use strict;
+use warnings;
+use Pod::Usage;
+use Data::Dumper;
+use Getopt::Long;
+use File::Spec;
+use DBI;
+
+### args/flags
+pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
+
+my ($verbose, $database_file);
+GetOptions(
+	   "database=s" => \$database_file,
+	   "verbose" => \$verbose,
+	   "help|?" => \&pod2usage # Help
+	   );
+
+### I/O error & defaults
+die " ERROR: provide a database file name!\n"
+	unless $database_file;
+die " ERROR: cannot find database file!\n"
+	unless -e $database_file;
+
+### MAIN
+# connect 2 db #
+my %attr = (RaiseError => 0, PrintError=>0, AutoCommit=>0);
+my $dbh = DBI->connect("dbi:SQLite:dbname=$database_file", '','', \%attr) 
+	or die " Can't connect to $database_file!\n";
+
+# database metadata #
+my $table_list_r = list_tables();
+check_for_loci_table($table_list_r);
+
+# loading loci table #
+my ($loci_r, $header_r) = load_loci_table();
+
+
+# updating / loading_db #
+load_new_entries($dbh, $loci_r->{"new_entry"}, $header_r);
+update_db($dbh, $loci_r, $header_r);
+
+# disconnect to db #
+$dbh->disconnect();
+exit;
+
+### Subroutines
+sub update_db{
+# updating any entries with CLI identifiers #
+	my ($dbh, $loci_r, $header_r) = @_;
+	
+	# entries ordered #
+	my @keys = keys %$header_r;
+	@keys = grep(!/^locus_id$/i, @keys);
+	my @values = @$header_r{@keys};
+
+	# setting up update for all columns #
+	my @set;
+	foreach my $key (@keys){
+		(my $tmp = $key) =~ s/$/ = ?/;
+		push @set, $tmp;
+		}
+
+	# preparing sql #	
+	my $cmd = join(" ", "UPDATE Loci SET", join(",", @set), "where locus_id = ?");
+	my $sql = $dbh->prepare($cmd);
+
+	# updating #
+	foreach my $entry (keys %$loci_r){
+		next if $entry eq "new_entry";
+		my $row = $loci_r->{$entry};
+		
+		$sql->execute( (@$row[@values], $$row[$header_r->{"locus_id"}]) );
+		
+		if($DBI::err){
+			print STDERR "ERROR: $DBI::errstr in: ", join("\t", @$row[@values]), "\n";
+			}
+		}
+	
+	$dbh->commit;	
+	
+	print STDERR "...Number of entries updated in loci table:", (scalar keys %$loci_r) -1, "\n"
+		unless $verbose;
+	}
+
+sub load_new_entries{
+# adding new entries to db #
+	my ($dbh, $loci_new_r, $header_r) = @_;
+
+	# making locus_id = NULL #
+	my @keys = keys %$header_r;
+	@keys = grep(!/^locus_id$/i, @keys);
+	my @values = @$header_r{@keys};
+
+	# loading entries #
+	my $cmd = "INSERT INTO loci(" . join(",", @keys) . ") VALUES(?".",?"x$#keys . ")";
+	my $sql = $dbh->prepare($cmd);
+	foreach my $row (@$loci_new_r){
+		$sql->execute( @$row[@values] );	
+		if($DBI::err){
+			print STDERR "ERROR: $DBI::errstr in: ", join("\t", @$row[@values]), "\n";
+			}
+		}
+	$dbh->commit;
+
+	print STDERR "...Number of entries added to loci table: ", scalar @$loci_new_r, "\n"
+		unless $verbose;
+	}
+
+sub load_loci_table{
+	my %loci;
+	my %header;
+	while(<>){
+		chomp;
+		next if /^\s*$/;
+
+		if($. == 1){ # loading header
+			tr/A-Z/a-z/; 						# all to lower case (caps don't matter)
+			my @line = split /\t/;
+			for my $i (0..$#line){
+				$header{$line[$i]} = $i;		# column_name => index
+				}
+			}
+		else{
+			my @line = split /\t/;
+			if( exists $header{"locus_id"} && $line[$header{"locus_id"}] ){		# if updating lci
+				$loci{$line[$header{"locus_id"}]} = \@line;
+				}
+			else{
+				push (@{$loci{"new_entry"}}, \@line);
+				}
+			}
+		}
+		#print Dumper %loci; exit; 
+	return (\%loci, \%header);;
+	}
+
+sub check_for_loci_table{
+	my ($table_list_r) = @_;
+	die " ERROR: loci table not found in database!\n"
+		unless grep(/^loci$/i, @$table_list_r);
+	}
+
+sub list_tables{
+	my $all = $dbh->selectall_hashref("SELECT tbl_name FROM sqlite_master", 'tbl_name');
+	return [keys %$all];
+	}
+
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Cdb_loadLoci.pl -- adding/updating loci entries in to CRISPR_db
+
+=head1 SYNOPSIS
+
+Cdb_loadLoci.pl [options] < loci_table.txt
+
+=head2 options
+
+=over
+
+=item -d 	sqlite3 database (required).
+
+=item -v	Verbose output. [TRUE]
+
+=item -h	This help message
+
+=back
+
+=head2 For more information:
+
+perldoc Cdb_loadLoci.pl
+
+=head1 DESCRIPTION
+
+Load and/or update CRISPR loci table entries.
+
+Loci entries will be added if no Locus_id is provided;
+otherwise, the entrie will be updated.
+
+=head1 EXAMPLES
+
+=head2 Usage:
+
+Cdb_loadLoci.pl -d CRISPR.sqlite < loci.txt
+
+=head1 AUTHOR
+
+Nick Youngblut <nyoungb2@illinois.edu>
+
+=head1 AVAILABILITY
+
+sharchaea.life.uiuc.edu:/home/git/CRISPR_db/
+
+=head1 COPYRIGHT
+
+Copyright 2010, 2011
+This software is licensed under the terms of the GPLv3
+
+=cut
+
---
+++ blib/script/Cdb_makeDB.pl
@@ -0,0 +1,194 @@
+#!/usr/bin/env perl
+
+### modules
+use strict;
+use warnings;
+use Pod::Usage;
+use Data::Dumper;
+use Getopt::Long;
+use File::Spec;
+
+### args/flags
+#pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
+
+my ($verbose, $replace);
+GetOptions(
+	   "replace" => \$replace,
+	   "verbose" => \$verbose,
+	   "help|?" => \&pod2usage # Help
+	   );
+
+### I/O error & defaults
+$ARGV[0] = "CRISPR.sqlite" unless $ARGV[0];
+
+### MAIN
+my $sql = get_sql();
+make_db($sql, $ARGV[0]);
+
+### Subroutines
+sub make_db{
+	my ($sql, $db_name) = @_;
+	if(-e $db_name){
+		if($replace){ unlink $db_name; }
+		else{ die " ERROR: $db_name already exists! Use '-r' to replace\n"; }
+		}
+			
+	open PIPE, "| sqlite3 $db_name" or die $!;
+	print PIPE $sql; 
+	close PIPE;
+	
+	print STDERR "...sqlite3 database tables created\n";
+	}
+
+sub get_sql{
+	my $sql = <<HERE;
+/* creating tables */
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS Loci;
+
+CREATE TABLE Loci (
+Locus_ID	INTEGER	PRIMARY KEY,
+Taxon_ID	TEXT	NOT NULL,
+Taxon_Name	TEXT	NOT NULL,
+Subtype	TEXT	NOT NULL,
+Locus_Start	INTEGER	NOT NULL,
+Locus_End	INTEGER	NOT NULL,
+Operon_Start	INTEGER	NOT NULL,
+Operon_End	INTEGER	NOT NULL,
+CRISPR_Array_Start	INTEGER	NOT NULL,
+CRISPR_Array_End	INTEGER	NOT NULL,
+Status	TEXT	NOT NULL,
+Genbank	TEXT	NOT NULL,
+Array_File	TEXT	NOT NULL,
+Scaffolds	INTEGER,
+File_Creation_Date	TEXT,
+Author	TEXT	NOT NULL
+);
+
+
+DROP TABLE IF EXISTS Loci_test;
+
+CREATE TABLE Loci_test (
+Locus_ID	integer	primary key	autoincrement,
+Taxon_ID	TEXT	NOT NULL,
+Taxon_Name	TEXT	NOT NULL
+);
+
+
+DROP TABLE IF EXISTS Spacers;
+
+CREATE TABLE Spacers (
+Locus_ID	INTEGER	PRIMARY KEY,
+Spacer_ID	TEXT	NOT NULL,
+Spacer_Start	INTEGER	NOT NULL,
+Spacer_End	INTEGER	NOT NULL,
+Spacer_Sequence	TEXT	NOT NULL,
+Spacer_Group	TEXT,
+UNIQUE (Locus_ID, Spacer_ID)
+);
+
+
+DROP TABLE IF EXISTS DirectRepeats;
+
+CREATE TABLE DirectRepeats (
+Locus_ID	INTEGER	PRIMARY KEY,
+Repeat_ID	INTEGER	NOT NULL,
+Repeat_Start	INTEGER	NOT NULL,
+Repeat_End	INTEGER	NOT NULL,
+Repeat_Sequence	TEXT	NOT NULL,
+Repeat_Group	INTEGER
+);
+
+
+DROP TABLE IF EXISTS DirectRepeatConsensus;
+
+CREATE TABLE DirectRepeatConsensus (
+Locus_ID	INTEGER	PRIMARY KEY,
+Repeat_Consensus_Sequence	TEXT	NOT NULL
+);
+
+
+DROP TABLE IF EXISTS LeaderSeqs;
+
+CREATE TABLE LeaderSeqs (
+Locus_ID	INTEGER	PRIMARY KEY,
+Leader_Start	INTEGER	NOT NULL,
+Leader_End	INTEGER	NOT NULL,
+Leader_Sequence	TEXT	NOT NULL,
+Leader_Group	TEXT
+);
+
+
+DROP TABLE IF EXISTS Genes;
+
+CREATE TABLE Genes (
+Locus_ID	INTEGER	PRIMARY KEY,
+Gene_ID	TEXT	NOT NULL,
+Gene_Start	INTEGER	NOT NULL,
+Gene_End	INTEGER	NOT NULL,
+Gene_Length__AA	INTEGER	NOT NULL,
+In_Operon	TEXT	NOT NULL,
+Gene_Alias	TEXT,
+UNIQUE (Locus_ID, Gene_ID)
+);
+
+
+COMMIT;
+
+HERE
+
+	return $sql;
+	}
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Cdb_makeDB.pl -- Initial DB construction
+
+=head1 SYNOPSIS
+
+Cdb_makeDB.pl [options] [DATABASE_name]
+
+=head2 options
+
+=over
+
+=item -r 	Replace existing database.
+
+=item -h	This help message
+
+=back
+
+=head2 For more information:
+
+perldoc Cdb_makeDB.pl
+
+=head1 DESCRIPTION
+
+Make all of the CRISPR_db tables.
+
+=head1 EXAMPLES
+
+=head2 Naming database 'CRISPR_db1'
+
+Cdb_makeDB.pl CRISPR_db1
+
+=head1 AUTHOR
+
+Nick Youngblut <nyoungb2@illinois.edu>
+
+=head1 AVAILABILITY
+
+sharchaea.life.uiuc.edu:/home/git/CRISPR_db/
+
+=head1 COPYRIGHT
+
+Copyright 2010, 2011
+This software is licensed under the terms of the GPLv3
+
+=cut
+
