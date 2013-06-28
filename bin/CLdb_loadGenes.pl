3b630de7c83722b1a338735e5442f186c9f2bed3
Nick Youngblut <nyoungb2@illinois.edu>
2013-06-28 13:17:57 -0500

    CLdb_loadGenes: v1

--- bin/CLdb_loadGenes.pl
+++ bin/CLdb_loadGenes.pl
@@ -32,22 +32,90 @@ my $dbh = DBI->connect("dbi:SQLite:dbname=$database_file", '','', \%attr)
 	or die " Can't connect to $database_file!\n";
 
 # database metadata #
-my $table_list_r = list_tables();
-check_for_loci_table($table_list_r);
+my $table_list_r = list_tables($dbh);
+check_for_genes_table($table_list_r);
+#get_genes_table_info($dbh);
 
-# loading loci table #
-#my ($loci_r, $header_r) = load_loci_table();
-
-
+# loading genes table #
+my ($genes_r, $header_r) = get_gene_table();
 
 # updating / loading_db #
-#load_new_entries($dbh, $loci_r->{"new_entry"}, $header_r);
-#update_db($dbh, $loci_r, $header_r);
+load_new_entries($dbh, $genes_r, $header_r);
 
 # disconnect to db #
 $dbh->disconnect();
 exit;
 
+
+### Subroutines
+sub load_new_entries{
+# adding new entries to db #
+## conflicts should be replaced (ie. updated ##
+	my ($dbh, $genes_r, $header_r) = @_;
+
+	# making locus_id = NULL #
+	my @keys = keys %$header_r;
+	@keys = grep(!/^sequence$/i, @keys);		# removed from table; not needed
+	my @values = @$header_r{@keys};
+
+	# loading entries #
+	my $cmd = "INSERT INTO genes(" . join(",", @keys) . ") VALUES(?".",?"x$#keys . ")";
+	my $sql = $dbh->prepare($cmd);
+	
+	my $cnt = 0;
+	foreach my $locus_id (keys %$genes_r){
+		foreach my $gene_id (keys %{$genes_r->{$locus_id}}){			
+			#	print Dumper @{$genes_r->{$locus_id}{$gene_id}}[@values]; exit;
+			$sql->execute( @{$genes_r->{$locus_id}{$gene_id}}[@values] );	
+			if($DBI::err){
+				print STDERR "ERROR: $DBI::errstr in: ", join("\t", @{$genes_r->{$locus_id}{$gene_id}}[@values]), "\n";
+				}
+			else{ $cnt++; }
+			}
+		}
+	$dbh->commit;
+
+	print STDERR "...Number of entries added/updated in gene table: $cnt\n"
+		unless $verbose;
+	}
+
+
+sub get_gene_table{
+	my %genes;
+	my %header;
+	while(<>){
+		chomp;
+		next if /^\s*$/;
+
+		if($. == 1){ # loading header
+			tr/A-Z/a-z/; 						# all to lower case (caps don't matter)
+			my @line = split /\t/;
+			for my $i (0..$#line){
+				$header{$line[$i]} = $i;		# column_name => index
+				}
+			}
+		else{
+			my @line = split /\t/;
+			$genes{$line[$header{"locus_id"}]}{$line[$header{"gene_id"}]} = \@line;
+			}
+		}
+		#print Dumper %header; exit;
+		#print Dumper %genes; exit; 
+	return (\%genes, \%header);;
+	}
+
+
+sub check_for_genes_table{
+	my ($table_list_r) = @_;
+	die " ERROR: Genes table not found in database!\n"
+		unless grep(/^Genes$/i, @$table_list_r);
+	}
+
+sub list_tables{
+	my ($dbh) = @_;
+	my $all = $dbh->selectall_hashref("SELECT tbl_name FROM sqlite_master", 'tbl_name');
+	return [keys %$all];
+	}
 
 __END__
 
@@ -55,17 +123,25 @@ __END__
 
 =head1 NAME
 
-template.pl -- script template
+Cldb_loadGenes.pl -- adding/updating gene table in CRISPR database
 
 =head1 SYNOPSIS
 
-template.pl [options] < input > output
+Cldb_loadGenes.pl [flags] < gene_table.txt
 
-=head2 options
+=head2 Required flags
 
 =over
 
-=item -v	Verbose output
+=item -d 	CRISPR database.
+
+=back 
+
+=head2 Optional flags
+
+=over
+
+=item -v	Verbose output. [TRUE]
 
 =item -h	This help message
 
@@ -73,24 +149,27 @@ template.pl [options] < input > output
 
 =head2 For more information:
 
-perldoc template.pl
+perldoc Cldb_loadGenes.pl
 
 =head1 DESCRIPTION
 
-The flow of execution is roughly:
-   1) Step 1
-   2) Step 2
-   3) Step 3
+Add/update genes entries in Genes table within the
+specified CRISPR database.
+
+A gene table made by CLdb_getGenesInLoci.pl can be
+piped directly into CLdb_loadGenes.pl, or 
+the aliases (or other values) can be currated (manually)
+first. 
 
 =head1 EXAMPLES
 
-=head2 Usage method 1
+=head2 Basic usage:
 
-template.pl <read1.fastq> <read2.fastq> <output directory or basename>
+Cldb_loadGenes.pl -d CRISPR.sqlite < genes_table.txt
 
-=head2 Usage method 2
+=head2 Piping from CLdb_getGenesInLoci.pl 
 
-template.pl <library file> <output directory or basename>
+CLdb_getGenesInLoci.pl -d CRISPR.sqlite | Cldb_loadGenes.pl -d CRISPR.sqlite
 
 =head1 AUTHOR
 
@@ -98,7 +177,7 @@ Nick Youngblut <nyoungb2@illinois.edu>
 
 =head1 AVAILABILITY
 
-sharchaea.life.uiuc.edu:/home/git/
+sharchaea.life.uiuc.edu:/home/git/CRISPR_db/
 
 =head1 COPYRIGHT
 
