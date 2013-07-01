2b44367b529843e3b44face86be0d6a28dffb8dc
Nick Youngblut <nyoungb2@illinois.edu>
2013-07-01 17:05:21 -0500

    getLeaderRegions: fixed gene overlap bug

--- bin/CLdb_getLeaderRegions.pl
+++ bin/CLdb_getLeaderRegions.pl
@@ -14,7 +14,7 @@ use Set::IntervalTree;
 ### args/flags
 pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
 
-my ($verbose, $database_file);
+my ($verbose, $database_file, $overlap_check);
 my $extra_query = "";
 my $genbank_path = "";
 my $length = 400;
@@ -23,6 +23,7 @@ GetOptions(
 	   "query=s" => \$extra_query, 
 	   "genbank=s" => \$genbank_path,
 	   "length=i" => \$length,
+	   "overlap" => \$overlap_check,
 	   "verbose" => \$verbose,
 	   "help|?" => \&pod2usage # Help
 	   );
@@ -101,7 +102,8 @@ sub get_leader_seq{
 				else{ die " LOGIC ERROR: $!\n"; }
 				
 				# checking for gene overlap #
-				($seqio, $leader_start, $leader_end) = check_gene_overlap($seqio, $leader_start, $leader_end, $loc, $locus);
+				($seqio, $leader_start, $leader_end) = check_gene_overlap($seqio, $leader_start, $leader_end, $loc, $locus)
+					unless $overlap_check;
 				
 				# parsing seq from genbank #
 				my $leader_seqio = $seqio->trunc($leader_start, $leader_end);					
@@ -117,6 +119,66 @@ sub get_leader_seq{
 	}
 
 sub check_gene_overlap{
+# checking to see if and genes overlap w/ leader region #
+# if yes, truncating leader region #
+	my ($seq, $region_start, $region_end, $leader_loc, $locus) = @_;
+	
+	# making an interval tree #
+	my $itree = Set::IntervalTree->new();
+	for my $feat ($seq->get_SeqFeatures){
+		next if $feat->primary_tag eq "source";
+
+		my $start = $feat->location->start;
+		my $end = $feat->location->end;
+		if($start <= $end){
+			$itree->insert($feat, $start - 1,  $end + 1);
+			}
+		else{
+			$itree->insert($feat, $end - 1,  $start + 1);		
+			}
+		}
+		
+	# debuggin itree #	
+	#	print_itree($itree, 500);
+
+	my $overlap_pos = 0;
+	if($leader_loc eq "start"){		# checking for 1st overlap from 3' - 5' end # 
+		for (my $i=$region_end; $i>=$region_start; $i--){
+			$overlap_pos = check_pos($itree, $i);
+			last if $overlap_pos;
+			}
+		}
+	elsif($leader_loc eq "end"){	# checking for 1st overlap from 5' - 3' end;
+		for (my $i=$region_start; $i<=$region_end; $i++){
+			$overlap_pos = check_pos($itree, $i);
+			last if $overlap_pos;
+			}
+		}
+	
+	sub check_pos{
+		my ($itree, $i) = @_;
+		my $res = $itree->fetch($i, $i);
+		return $i if $$res[0];
+		}
+		
+	# altering start-end #
+	if($overlap_pos){
+		print STDERR "WARNING: Gene(s) overlap the leader in cli.$locus array. Truncating\n" unless $verbose;
+		
+		if($leader_loc eq "start"){
+			$region_start = $overlap_pos;
+			}
+		elsif($leader_loc eq "end"){
+			$region_end = $overlap_pos;
+			}
+		die " LOGIC ERROR: region start is > region end! $!\n"
+			if $region_start > $region_end;
+		}
+		
+	return $seq, $region_start, $region_end;
+	}
+
+sub check_gene_overlap_old{
 # checking to see if and genes overlap w/ leader region #
 # if yes, truncating leader region #
 	my ($seq, $region_start, $region_end, $leader_loc, $locus) = @_;
@@ -188,8 +250,7 @@ sub print_itree{
 	
 	for my $i (0..$end){
 		my $res = $itree->fetch($i, $i);
-#		print Dumper $$res[0] if $$res[0];
-#		print join("\t", "pos:$i", join("::", @$res)), "\n";
+		print join("\t", "pos:$i", join("::", @$res)), "\n";
 		}
 	exit;
 	}	
@@ -315,6 +376,8 @@ CLdb_getLeaderRegion.pl [flags]
 =item -l 	Maximum length of leader region (bp). [400]
 
 =item -q 	sql to refine loci query (see EXAMPLES).
+
+=item -o 	Check for overlapping genes (& trim region if overlap)? [TRUE]
 
 =item -v	Verbose output
 
