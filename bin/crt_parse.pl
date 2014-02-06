16a70b173758dc01fe0a58d395e218c3b357830c
Nick Youngblut <nyoungb2@illinois.edu>
2014-02-05 19:27:59 -0600

    crt_parse: v1

---
+++ bin/crt_parse.pl
@@ -0,0 +1,154 @@
+#!/usr/bin/env perl
+
+=pod
+
+=head1 NAME
+
+crt_parse.pl -- parsing output from CRISPR Recognition Tool (CRT); convert to CRISPRFinder format
+
+=head1 SYNOPSIS
+
+crt_parse.pl [flags] < crt.out
+
+=head2 Required flags
+
+=over
+
+NONE
+
+=back
+
+=head2 Optional flags
+
+=over
+
+=item -p	Output file(s) prefix. [array]
+
+=item -v	Verbose output
+
+=item -h	This help message
+
+=back
+
+=head2 For more information:
+
+perldoc crt_parse.pl
+
+=head1 DESCRIPTION
+
+Parse the putative CRISPR arrays identified by 
+the CRISPR Recognition Tool (CRT)
+and convert to CRISPRFinder format.
+
+Each array will be written to a separate file.
+
+File naming: 'prefix'_'CRISPR#'_'start'_'end'
+
+start-end is in relation to the positive strand.
+
+=head1 EXAMPLES
+
+=head2 Basic usage (CRT output file = 'a.out'):
+
+crt_parse.pl < a.out
+
+=head1 AUTHOR
+
+Nick Youngblut <nyoungb2@illinois.edu>
+
+=head1 AVAILABILITY
+
+sharchaea.life.uiuc.edu:/home/git/CLdb/
+
+=head1 COPYRIGHT
+
+Copyright 2010, 2011
+This software is licensed under the terms of the GPLv3
+
+=cut
+
+
+#--- modules ---#
+use strict;
+use warnings;
+use Pod::Usage;
+use Data::Dumper;
+use Getopt::Long;
+use File::Spec;
+
+#--- args/flags ---#
+pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
+
+my ($verbose_b);
+my $prefix = "array";
+GetOptions(
+	"prefix=s" => \$prefix, 
+	"verbose" => \$verbose_b,
+	"help|?" => \&pod2usage # Help
+	);
+
+#--- I/O error ---#
+
+
+#--- MAIN ---#
+my $res_r = parse_crt();
+write_array_files($res_r, $prefix);
+
+#--- Subroutines ---#
+sub write_array_files{
+	my ($res_r, $prefix) = @_;
+	
+	foreach my $array (keys %$res_r){
+		my $outname = join("_", $prefix, $array, 
+					$res_r->{$array}{'start'}, $res_r->{$array}{'end'});
+		open OUT, ">$outname.txt" or die $!;
+		foreach my $line (@{$res_r->{$array}{'array'}}){
+			print OUT join("\t", @$line), "\n";
+			}
+		close OUT;
+		
+		print STDERR "File written: $outname.txt\n";
+		}
+	}
+
+sub parse_crt{
+# parsing CRT output 
+	my %res;
+	while(<>){
+		chomp;
+			
+		if(/^CRISPR \d+/){
+			my @head = split / +/;	# 1=num; 3=start; 5=end
+			die "ERROR: line $. is not formatted correctly!\n"
+				unless scalar @head == 6;
+			
+			$res{$head[1]}{'start'} = $head[3];
+			$res{$head[1]}{'end'} = $head[5];
+			die "ERROR: start > end at line $.\n"
+				unless $head[3] <= $head[5];
+			
+			while(<>){
+				chomp;
+				last if /^Repeats:/;
+				next if /^(POSITION|^-)/;
+				
+				my @line = split /\t+/;
+				die "ERROR: line $. does not have 2 or 4 tab-delimited columns!\n"
+					unless scalar @line == 2 || scalar @line == 4;
+
+				# calculating end #
+				my ($DR_len, $spacer_len) = (0,0);
+				$DR_len = length $line[1] if defined $line[1];
+				$spacer_len = length $line[2] if defined $line[2];				
+				$line[3] = $line[0] + $DR_len + $spacer_len - 1;
+				
+				$line[2] = "\t" unless defined $line[2];
+				push @{$res{$head[1]}{'array'}}, \@line;
+				}
+			}
+		}
+		
+		#print Dumper %res; exit;
+	return \%res;
+	}
+
