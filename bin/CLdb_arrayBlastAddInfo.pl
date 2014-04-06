c56b9eeb17353c8957f791b370fd9f0910e6bfb6
Nick Youngblut <nyoungb2@illinois.edu>
2014-04-06 16:53:44 -0500

    arrayBlastAddInfo: parallel::ForkManager

--- bin/CLdb_arrayBlastAddInfo.pl
+++ bin/CLdb_arrayBlastAddInfo.pl
@@ -44,6 +44,10 @@ Add start-stop position? [FALSE]
 
 Append subject blast DB file name to subject value? [FALSE]
 
+=item -fork  <int>
+
+Number blast hits to process in parallel. [1]
+
 =item -order  <bool>
 
 Add spacer-leader order? [FALSE]
@@ -64,6 +68,8 @@ perldoc CLdb_arrayBlastAddInfo.pl
 Add CLdb info to spacer or DR IDs (locusID|spacer/DR|spacer/DR_ID|groupID).
 Also, all blast hit entries for a spacer/DR group will be
 duplicated for each individual spacer/DR in the spacer/DR group (replication).
+
+Even blast entries with no hits (just comment lines) will be processed.
 
 =head1 EXAMPLES
 
@@ -99,6 +105,7 @@ use Data::Dumper;
 use Getopt::Long;
 use File::Spec;
 use DBI;
+use Parallel::ForkManager;
 
 # CLdb #
 use FindBin;
@@ -121,6 +128,7 @@ pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
 
 my ($verbose, $database_file);
 my ($subtype_b, $taxon_id_b, $taxon_name_b, $pos_b, $order_b, $append_b);
+my $forks = 0;
 GetOptions(
 	   "database=s" => \$database_file,
 	   "subtype" => \$subtype_b,
@@ -128,6 +136,7 @@ GetOptions(
 	   "taxon_name" => \$taxon_name_b,
 	   "position" => \$pos_b,
 	   "order" => \$order_b,
+	   "fork=i" => \$forks,
 	   "append_db" => \$append_b,
 	   "verbose" => \$verbose,
 	   "help|?" => \&pod2usage # Help
@@ -142,7 +151,7 @@ my $dbh = connect2db($database_file);
 
 # checking table existence #
 table_exists($dbh, "loci"); 
-#table_exists($dbh, "leaders") if $leader_b;
+$dbh->disconnect();
 
 # loading file #
 my ($lines_r) = read_blast_file();
@@ -152,7 +161,7 @@ my ($lines_r) = read_blast_file();
 my $query = add_query_opts($subtype_b, $taxon_id_b, $taxon_name_b);
 
 ## querying ##
-my $index_r = get_CLdb_info($dbh, $lines_r, $query);
+my $index_r = get_CLdb_info($database_file, $lines_r, $query, $forks);
 
 # writing edited fasta #
 write_blast_file($index_r, $lines_r);
@@ -165,72 +174,72 @@ exit;
 
 ### Subroutines
 sub write_blast_file{
-	my ($index_r, $lines_r) = @_;
-	
-	foreach my $query (sort keys %$lines_r){
-		(my $q = $query) =~ s/^# Query:\s([^|]+\|(spacer|DR)\|[^|]+\|[^|]+).*/$1/;
-		foreach my $db (sort keys %{$lines_r->{$query}}){
-		  my $db_name = get_db_name($db) if $append_b; # appending db_name onto subject
-		       foreach my $blast (keys %{$lines_r->{$query}{$db}}){
-				if(exists $index_r->{$q}){		# spacer group
-					# duplicating blast hits #
-					foreach my $new_query (@{$index_r->{$q}}){
-						print $blast, "\n";
-						print "# Query: $new_query\n";						
-						print $db, "\n";
-						print $lines_r->{$query}{$db}{$blast}{'fields'}, "\n"
-							if exists $lines_r->{$query}{$db}{$blast}{'fields'};
-						print join("\n", @{$lines_r->{$query}{$db}{$blast}{'comments'}}), "\n";
-						# printing each hit #
-						foreach my $l (@{$lines_r->{$query}{$db}{$blast}{'hits'}}){
-							my @l = split /\t/, $l;
-							
-							# changing query ID column #
-							die "ERROR: cannot find 'query id' for '$l'\n"
-								unless exists $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'query id'};
-							my $query_id_i = $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'query id'};
-							$l[$query_id_i] = $new_query;	
-							
-							# changing subject ID column if append_b #
-							if(defined $db_name){
-							  die "ERROR: cannot find 'subject id' for '$l'\n"
-							    unless exists  $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'subject id'};
-							  my $subject_id_i = $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'subject id'};
-							  $l[$subject_id_i] = join("|", $db_name, $l[$subject_id_i]);
-							}
+  my ($index_r, $lines_r) = @_;
+  
+  foreach my $query (sort keys %$lines_r){
+    (my $q = $query) =~ s/^# Query:\s([^|]+\|(spacer|DR)\|[^|]+\|[^|]+).*/$1/;
+    foreach my $db (sort keys %{$lines_r->{$query}}){
+      my $db_name = get_db_name($db) if $append_b; # appending db_name onto subject
+      foreach my $blast (keys %{$lines_r->{$query}{$db}}){
+	if(exists $index_r->{$q}){		# spacer group
+	  # duplicating blast hits #
+	  foreach my $new_query (@{$index_r->{$q}}){
+	    print $blast, "\n";
+	    print "# Query: $new_query\n";						
+	    print $db, "\n";
+	    print $lines_r->{$query}{$db}{$blast}{'fields'}, "\n"
+	      if exists $lines_r->{$query}{$db}{$blast}{'fields'};
+	    print join("\n", @{$lines_r->{$query}{$db}{$blast}{'comments'}}), "\n";
+	    # printing each hit #
+	    foreach my $l (@{$lines_r->{$query}{$db}{$blast}{'hits'}}){
+	      my @l = split /\t/, $l;
+	      
+	      # changing query ID column #
+	      die "ERROR: cannot find 'query id' for '$l'\n"
+		unless exists $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'query id'};
+	      my $query_id_i = $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'query id'};
+	      $l[$query_id_i] = $new_query;	
+	      
+	      # changing subject ID column if append_b #
+	      if(defined $db_name){
+		die "ERROR: cannot find 'subject id' for '$l'\n"
+		  unless exists  $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'subject id'};
+		my $subject_id_i = $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'subject id'};
+		$l[$subject_id_i] = join("|", $db_name, $l[$subject_id_i]);
+	      }
 							# writing output
-							print join("\t", @l), "\n";
-							}
-						}				
+	      print join("\t", @l), "\n";
+	    }
+	  }				
 					}
-				else{		# individual spacer
-					print $blast, "\n";
-					print $query, "\n";
-					print $db, "\n";
-					print $lines_r->{$query}{$db}{$blast}{'fields'}, "\n"
-						if exists $lines_r->{$query}{$db}{$blast}{'fields'};
-					print join("\n", @{$lines_r->{$query}{$db}{$blast}{'comments'}}), "\n";
-					
-					# changing subject ID column if append_b
-					if(defined $db_name){
-					  foreach my $l (@{$lines_r->{$query}{$db}{$blast}{'hits'}}){
-					    my @l = split /\t/, $l;
-					    die "ERROR: cannot find 'subject id' for '$l'\n"
-					      unless exists  $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'subject id'};
-					    my $subject_id_i = $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'subject id'};
-					    $l[$subject_id_i] = join("|", $db_name, $l[$subject_id_i]);
-					    print join("\t", @l), "\n";
-					  }
+	else{		# individual spacer
+	  print $blast, "\n";
+	  print $query, "\n";
+	  print $db, "\n";
+	  print $lines_r->{$query}{$db}{$blast}{'fields'}, "\n"
+	    if exists $lines_r->{$query}{$db}{$blast}{'fields'};
+	  print join("\n", @{$lines_r->{$query}{$db}{$blast}{'comments'}}), "\n";
+	  
+	  # changing subject ID column if append_b
+	  if(defined $db_name){
+	    foreach my $l (@{$lines_r->{$query}{$db}{$blast}{'hits'}}){
+	      my @l = split /\t/, $l;
+	      die "ERROR: cannot find 'subject id' for '$l'\n"
+		unless exists  $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'subject id'};
+	      my $subject_id_i = $lines_r->{$query}{$db}{$blast}{'fields_sep'}{'subject id'};
+	      $l[$subject_id_i] = join("|", $db_name, $l[$subject_id_i]);
+	      print join("\t", @l), "\n";
+	    }
+	  }
+	  else{
+	    # printing all hits #
+	    print join("\n", @{$lines_r->{$query}{$db}{$blast}{'hits'}}), "\n";
 					}
-					else{
-					  # printing all hits #
-					  print join("\n", @{$lines_r->{$query}{$db}{$blast}{'hits'}}), "\n";
-					}
-					}
-				}
-			}
-		}
 	}
+      }
+    }
+  }
+}
 
 sub get_db_name{
  # getting just the file name of the blast DB used 
@@ -245,88 +254,108 @@ sub get_db_name{
 sub get_CLdb_info{
 # getting necessary info from CLdb #
 ## query is sequence-dependent ##
-	my ($dbh, $lines_r, $query) = @_;
-	
-	my %res;
-	my $proc_cnt = 0;
-	foreach my $l (keys %$lines_r){
+  my ($database_file, $lines_r, $query, $forks) = @_;
 
-		next unless ($l =~ /^# Query:\s[^|]+\|(spacer|DR)\|[^|]+\|[^|]+.*/i);
-
-		# processing line w/ name #
-		(my $name = $l) =~ s/^# Query:\s([^|]+\|(spacer|DR)\|[^|]+\|[^|]+).*/$1/;
-		my @name = split /\|/, $name;
+  #forking
+  my $pm = Parallel::ForkManager->new($forks);
+  my %res;
+  $pm->run_on_finish(
+      sub{
+	my ($pid, $exit_code, $ident, $exit_signal, $core_dump, $ret_r) = @_;	
 		
-		# sanity check #
-		die "ERROR: '$name' does not have 4 components separated by '|'\n"
-			unless scalar @name == 4;
-		
-		# making sql #
-		my $cmd;
-		if($name[3] eq 'NA'){			# if no clusterID
-			$cmd = query_noGroup(\@name, $query);
-			}
-		else{							# groupID present 
-			$cmd = query_byGroup(\@name, $query);
-			}
-	
-		$cmd =~ s/[\n\t]+/ /g;
-		$cmd =~ s/ +/ /g;
-	
-		my $ret = $dbh->selectall_arrayref($cmd);
-		die "ERROR: no matching entries!\n"
-			unless $$ret[0];
-		
-		# loading hash #
-		#$res{$seq}{"data"} = $ret;
-		#$res{$seq}{"seq"} = $fasta_r->{$seq};
-		
-		# loading hash #
-		foreach my $x (@$ret){
-			my $new_name = join("|", @$x);
-			push @{$res{$name}}, $new_name;
-			}
-
-		# status #
-		$proc_cnt++;
-		print STDERR "Number of hits processed: $proc_cnt\n"
-		  if ! $verbose and $proc_cnt % 10 == 0;
-		}
-
-		#print Dumper %res; exit;
-	return \%res;
+	# loading hash #
+	foreach my $name (keys %$ret_r){
+	  foreach my $x ( @{$ret_r->{$name}} ){
+	    my $new_name = join("|", @$x);
+	    push @{$res{$name}}, $new_name;
+	  }
 	}
+      }
+  );
+
+  my $proc_cnt = 0;
+  foreach my $l (keys %$lines_r){
+    # status
+    $proc_cnt++;
+    print STDERR "Number of hits processed: $proc_cnt\n"
+      if ! $verbose and $proc_cnt % 100 == 0;    
+    
+    next unless ($l =~ /^# Query:\s[^|]+\|(spacer|DR)\|[^|]+\|[^|]+.*/i);
+
+    # forking
+    $pm->start and next;
+    
+    # connect to db
+    my $dbh = connect2db($database_file);
+
+    # processing line w/ name #
+    (my $name = $l) =~ s/^# Query:\s([^|]+\|(spacer|DR)\|[^|]+\|[^|]+).*/$1/;
+    my @name = split /\|/, $name;
+    
+    # sanity check #
+    die "ERROR: '$name' does not have 4 components separated by '|'\n"
+      unless scalar @name == 4;
+    
+    # making sql #
+    my $cmd;
+    if($name[3] eq 'NA'){			# if no clusterID
+      $cmd = query_noGroup(\@name, $query);
+    }
+    else{							# groupID present 
+      $cmd = query_byGroup(\@name, $query);
+    }
+    
+    $cmd =~ s/[\n\t]+/ /g;
+    $cmd =~ s/ +/ /g;
+    
+    my $ret = $dbh->selectall_arrayref($cmd);
+    die "ERROR: no matching entries!\n"
+      unless $$ret[0];
+    
+    my %tmp;
+    %tmp = ($name => $ret);
+
+
+    # disconnect from db and finish
+    $dbh->disconnect();
+
+    $pm->finish(0, \%tmp);
+  }
+  $pm->wait_all_children;
+
+  return \%res;
+}
 	
 sub query_noGroup{
-	my ($seq_r, $query) = @_;
-	
-	# table #
-	my $tbl_oi = "Spacers";
-	$tbl_oi = "DRs" if $$seq_r[1] =~ /DR/i;
-		
-	my $prefix = $$seq_r[1];			# "spacer|DR"
-	my $cmd = "SELECT 
+  my ($seq_r, $query) = @_;
+  
+  # table #
+  my $tbl_oi = "Spacers";
+  $tbl_oi = "DRs" if $$seq_r[1] =~ /DR/i;
+  
+  my $prefix = $$seq_r[1];			# "spacer|DR"
+  my $cmd = "SELECT 
 $tbl_oi.Locus_ID,
 '$prefix',
 $tbl_oi.$prefix\_ID,
 'NA'";
-	
-	# adding other query options #
-	$cmd .= ", $query" if $query;
-	if($pos_b){
-		$cmd .= ", loci.Scaffold";
-		$cmd .= ", $tbl_oi.$prefix\_start";
-		$cmd .= ", $tbl_oi.$prefix\_end";
+  
+  # adding other query options #
+  $cmd .= ", $query" if $query;
+  if($pos_b){
+    $cmd .= ", loci.Scaffold";
+    $cmd .= ", $tbl_oi.$prefix\_start";
+    $cmd .= ", $tbl_oi.$prefix\_end";
 		}	
-	
+  
 	# where statement #
-	$cmd .= "
+  $cmd .= "
 FROM $tbl_oi, loci
 WHERE loci.locus_id = $tbl_oi.locus_id";
-	
-	return $cmd;
-	}
-	
+  
+  return $cmd;
+}
+
 sub query_byGroup{
 	my ($seq_r, $query) = @_;
 	
