991c4236672dc704024eba9e36d49ccf4a6cb25c
Nick Youngblut <nyoungb2@illinois.edu>
2014-04-27 20:27:40 -0500

    CLdb_arrayBlastAddProto: updated & tested

--- bin/CLdb_arrayBlastAddFullQuery.pl
+++ bin/CLdb_arrayBlastAddFullQuery.pl
@@ -4,46 +4,25 @@
 
 =head1 NAME
 
-CLdb_arrayBlastAddInfo.pl -- add CLdb info to spacer or DR IDs ('locusID|spacer/DR|spacer/DR_ID|groupID') in Blast file
+CLdb_arrayBlastAddFullQuery -- adding the full query sequence to the blast table
 
 =head1 SYNOPSIS
 
-CLdb_arrayBlastAddInfo.pl [flags] < blast_results.txt > blast_results_info.txt
+CLdb_arrayBlastAddFullQuery.pl [flags] < blast_results.txt > blast_results_info.txt
 
 =head2 Required flags
 
 =over
 
-=item -database  <char>
+=item -fasta  <char>
 
-CLdb database.
+fasta file of spacer query sequences used for BLASTn
 
 =back
 
 =head2 Optional flags
 
 =over
-
-=item -subtype  <bool>
-
-Add subtype? [FALSE]
-
-=item -taxon_id  <bool>
-
-Add taxon_id? [FALSE]
-
-=item -taxon_name  <bool>
-
-Add taxon_name? [FALSE]
-
-=item -position  <bool>
-
-Add start-stop position? [FALSE]
-
-=item -order  <bool>
-
-Add spacer-leader order? [FALSE]
-# not implemented yet! #
 
 =item -v 	Verbose output. [FALSE]
 
@@ -53,23 +32,15 @@ Add spacer-leader order? [FALSE]
 
 =head2 For more information:
 
-perldoc CLdb_arrayBlastAddInfo.pl
+perldoc CLdb_arrayBlastAddFullQuery.pl
 
 =head1 DESCRIPTION
 
-Add CLdb info to spacer or DR IDs (locusID|spacer/DR|spacer/DR_ID|groupID).
-Also, all blast hit entries for a spacer/DR group will be
-duplicated for each individual spacer/DR in the spacer/DR group (replication).
+Adding the full query sequence to the spacer blast hits table.
+The blast table should be formatted as '-outfmt 7'
 
 =head1 EXAMPLES
 
-=head2 Add spacer subtype and position (BLAST hits will be replicated for each individual element in the group)
-
-CLdb_arrayBlastAddInfo.pl -d CLdb.sqlite -sub -pos < spacer-group_blast.txt > spacers_ALL_blast.txt
-
-=head2 Add DR subtype and position (BLAST hits will be replicated for each individual element in the group)
-
-CLdb_arrayBlastAddInfo.pl -d CLdb.sqlite -sub -pos < DR-group_blast.txt > DR_ALL_blast.txt
 
 =head1 AUTHOR
 
@@ -100,12 +71,8 @@ use DBI;
 use FindBin;
 use lib "$FindBin::RealBin/../lib";
 use lib "$FindBin::RealBin/../lib/perl5/";
-use CLdb::query qw/
-	table_exists
-	n_entries/;
 use CLdb::utilities qw/
-	file_exists 
-	connect2db/;
+	file_exists/;
 use CLdb::seq qw/
 	read_fasta/;
 use CLdb::blast qw/
@@ -124,11 +91,12 @@ GetOptions(
 	   );
 
 #--- I/O error & defaults ---#
+die "ERROR: provide a fasta file of the query sequences\n"
+  unless defined $fasta_in;
 file_exists($fasta_in, "fasta");
 
+
 #--- MAIN ---#
-
-
 # loading files #
 ## fasta of query sequences ##
 my $fasta_r = read_fasta($fasta_in);
@@ -144,88 +112,38 @@ write_blast_file($lines_r);
 
 
 
-### Subroutines
-sub write_blast_file_OLD{
-	my ($lines_r) = @_;
-	
-	foreach my $query ( sort keys %$lines_r ){
-		foreach my $db ( sort keys %{$lines_r->{$query}} ){
-			foreach my $blast ( keys %{$lines_r->{$query}{$db}} ){
-				print $blast, "\n";
-				print $query, "\n";
-				print $db, "\n";
-				print $lines_r->{$query}{$db}{$blast}{'fields'}, "\n"
-					if exists $lines_r->{$query}{$db}{$blast}{'fields'};
-				print join("\n", @{$lines_r->{$query}{$db}{$blast}{'comments'}}), "\n";
-				# printing all hits #
-				print join("\n", @{$lines_r->{$query}{$db}{$blast}{'hits'}}), "\n"
-					if exists $lines_r->{$query}{$db}{$blast}{'hits'};
-				}
-			}
-		}
-	}
-
+#--- Subroutines ---#
 sub add_full_query_seq{
 # adding full length query sequence to blast table #
-	my ($lines_r, $fasta_r) = @_;
+  my ($lines_r, $fasta_r) = @_;
 	
-	foreach my $query ( keys %$lines_r ){
-		
-		# checking for existence of query in fasta #
-		(my $name = $query) =~ s/# Query: //i;
-		unless (exists $fasta_r->{$name}){
-			warn "WARNING: '$name' not found in fasta. Skipping!\n";
-			next;
-			}
-		
-		# adding sequence to blast table #
-		foreach my $db ( keys %{$lines_r->{$query}} ){
-			
-			foreach my $blast ( keys %{$lines_r->{$query}{$db}} ){
-				next unless exists $lines_r->{$query}{$db}{$blast}{'fields'};
-				
-				$lines_r->{$query}{$db}{$blast}{'fields'} .= ", query_seq_full";
-				$lines_r->{$query}{$db}{$blast}{'fields_sep'}{"query_seq_full"} = 
-					scalar keys %{$lines_r->{$query}{$db}{$blast}{'fields_sep'}};
-				
-				foreach my $hit ( @{$lines_r->{$query}{$db}{$blast}{'hits'}} ){
-					$hit .= "\t$fasta_r->{$name}";
-					}
-				}
-			}
-		}
-		#print Dumper $lines_r; exit
-	}
+  foreach my $query ( keys %$lines_r ){
+	  
+    # checking for existence of query in fasta #
+    (my $name = $query) =~ s/# Query: //i;
+    unless (exists $fasta_r->{$name}){
+      warn "WARNING: '$name' not found in fasta. Skipping!\n";
+      next;
+    }
+    
+    # adding sequence to blast table #
+    foreach my $db ( keys %{$lines_r->{$query}} ){
+      
+      foreach my $blast ( keys %{$lines_r->{$query}{$db}} ){
+	next unless exists $lines_r->{$query}{$db}{$blast}{'fields'};
 	
-sub read_blast_file_OLD{
-# reading in each blast entry & extracting names and line numbers #
-	my %lines;
-	my $blast;
-	my $query;
-	my $db;
-	while(<>){
-		chomp;
-		
-		if(/^# BLAST/i){
-			$blast = $_;
-			}
-		elsif(/^# Query/i){
-			$query = $_;
-			}
-		elsif(/^# Database/i){
-			$db = $_;
-			}	
-		elsif(/^# /){
-			push @{$lines{$query}{$db}{$blast}{'comments'}}, $_;
-			}
-		else{
-			push @{$lines{$query}{$db}{$blast}{'hits'}}, $_;
-			}
-			
-		}		
-		#print Dumper %lines; exit;	
-	return \%lines;
+	$lines_r->{$query}{$db}{$blast}{'fields'} .= ", query_seq_full";
+	$lines_r->{$query}{$db}{$blast}{'fields_sep'}{"query_seq_full"} = 
+	  scalar keys %{$lines_r->{$query}{$db}{$blast}{'fields_sep'}};
+		    
+	foreach my $hit ( @{$lines_r->{$query}{$db}{$blast}{'hits'}} ){
+	  $hit .= "\t$fasta_r->{$name}";
 	}
+      }
+    }
+  }
+  #print Dumper $lines_r; exit
+}
 
 
 
--- bin/CLdb_arrayBlastAddProto.pl
+++ bin/CLdb_arrayBlastAddProto.pl
@@ -104,7 +104,7 @@ limited to the end of the genomic sequence.
 
 =head2 WARNING
 
-The spacer blast table must have comment lines!
+The spacer blast table must have comment lines (blastn -outfmt 7)!
 
 The blast databases must be in the location specified
 in the "# Database:" comment lines! 
@@ -155,10 +155,14 @@ use FindBin;
 use lib "$FindBin::RealBin/../lib";
 use lib "$FindBin::RealBin/../lib/perl5/";
 use CLdb::blast qw/
-	parse_blast_hits/;
+		    parse_blast_hits/;
 use CLdb::seq qw/
-	revcomp/;
-
+		  revcomp/;
+use CLdb::arrayBlast::addProto qw/
+				   get_proto_seq
+				   add_new_fields
+				   write_editted_blast
+				 /;
 
 
 ### args/flags
@@ -191,356 +195,39 @@ my ($blast_r, $fields_r) = parse_blast_hits();
 
 # getting protospacer & extensions #
 ## making list of protospacer/extension start-stop, & strand ##
-my $new_fields_r = get_proto_seq($blast_r, $fields_r, $extend, $len_cut);
+my $new_fields_r = get_proto_seq($blast_r, $fields_r, $extend, 
+				 $len_cut, \@pam, $verbose);
 
 # writing editted table #
 ## adding new fields ##
 add_new_fields($fields_r, $new_fields_r);
+
+#print Dumper $blast_r; exit;
+
 ## writing table ##
 write_editted_blast($blast_r, $fields_r);
 
 
 #--- Subroutines ---#
-sub write_editted_blast{
-# writing editted blast table #
-## making sure to update fields with appended ones ##
-	my ($blast_r, $fields_r) = @_;
-	
-	foreach my $query (sort keys %$blast_r){
-		foreach my $db (sort keys %{$blast_r->{$query}}){
-			# getting nubmer of hits passing filtering  # # skipping hit if no adding info (did not pass filtering) #
-			my $hit_cnt = 0;
-			foreach my $row (@{$blast_r->{$query}{$db}{"hits"}}){
-				$hit_cnt++ if $$row[$fields_r->{"proto_seq_fullx"}];
-				}
-			next if $hit_cnt == 0;
-
-			# modifying & writing header #
-			my $header_r;
-			foreach my $row (@{$blast_r->{$query}{$db}{"header"}}){
-				$row = new_header($row, $fields_r) if $row =~ /^# Field/;
-				print $row, "\n";
-				}			
-
-			# writing hits #
-			foreach my $row (@{$blast_r->{$query}{$db}{"hits"}}){
-				next unless $$row[$fields_r->{"proto_seq_fullx"}];
-				print join("\t", @$row), "\n";
-				}			
-			}
-		}	
-	}
-	
-sub new_header{
-	my ($row, $fields_r) = @_;
-	my $fields = join(", ", sort{$fields_r->{$a}<=>$fields_r->{$b}} keys %$fields_r);
-	return join(" ", "# Fields:", $fields);
-	}
-	
-sub add_new_fields{
-# adding new fields for output #
-	my ($fields_r, $new_fields_r) = @_;
-	
-	my $max_i = max(values %$fields_r);	# starting after last fields index
-	
-	for my $i (0..$#$new_fields_r){
-		$fields_r->{$$new_fields_r[$i]} = $i+$max_i+1;
-		}
-		#print Dumper %$fields_r; exit;
-	}
-
-sub get_proto_seq{
-# getting protospacer/extension start-stop & strand #
-	($blast_r, $fields_r, $extend, $len_cut) = @_;
-	
-	my %filter_sum;
-	foreach my $query (sort keys %$blast_r){
-		print STDERR "Processing query: '$query'\n" unless $verbose;
-		foreach my $db (keys %{$blast_r->{$query}}){
-			foreach my $row (@{$blast_r->{$query}{$db}{"hits"}}){
-				# checking existence of start-stop, qlen, slen #
-				check_values($row, $fields_r);	
-				
-				# filtering #
-				$filter_sum{"total"}++;
-				## deleting  hits with gaps ##
-				if ($$row[$fields_r->{"BTOP"}] =~ /-/){ 	# gaps in blast alignment
-					$filter_sum{"gaps"}++;
-					next;
-					}
-				## deleting if hit_len/query_len < $len_cut ##
-				if( ($$row[$fields_r->{"q. end"}] - $$row[$fields_r->{"q. start"}]) / 
-							$$row[$fields_r->{"query length"}] < $len_cut ){
-					$filter_sum{"length"}++;
-					next;
-					}
-				
-				# getting subject start-stop, strand #
-				my $Sstart = $$row[$fields_r->{"s. start"}];
-				my $Send = $$row[$fields_r->{"s. end"}];
-				my $strand;
-				if($Sstart > $Send){ $strand = 'minus'; }
-				else{ $strand = 'plus'; }
-				
-				# flipping Sstart-Send to + if - strand #
-				($Sstart, $Send) = ($Send, $Sstart) if $strand eq 'minus';
-					
-				# calling blastdbcmd #
-				## sequence will be - strand if $strand eq 'minus' #
-				my $proto_seq = call_blastdbcmd($$row[$fields_r->{"subject id"}],
-								$Sstart, $Send, $strand, $db, 1);			 # inverted so it is protospacer match
-				
-				# extending hit to full length & getting sequence #
-				## sstart-send, qstart-qend full length (as much as extension would allow) ##
-				my ($Sstart_full, $Send_full, $Qstart_full, $Qend_full) = 
-						extend_full_len($Sstart, $Send, $strand,
-									$$row[$fields_r->{"q. start"}],
-									$$row[$fields_r->{"q. end"}],
-									$$row[$fields_r->{"query length"}],
-									$$row[$fields_r->{"subject length"}]);
-
-					#print Dumper $Sstart_full, $Send_full, $Qstart_full, $Qend_full; exit;
-
-				my $proto_seq_full = call_blastdbcmd($$row[$fields_r->{"subject id"}],
-								$Sstart_full, $Send_full, $strand, $db, 1);		# not inverted; used for alignment
-				
-				# extending hit beyond full length (-x) #	
-				my ($Sstart_fullx, $Send_fullx) = extend_beyond_full($Sstart_full, $Send_full, $strand,
-													$$row[$fields_r->{"subject length"}], $extend);
-				
-				# calling blastdbcmd #
-				## sequence will be - strand if $strand eq 'minus' ##
-				my $proto_seq_full_x = call_blastdbcmd($$row[$fields_r->{"subject id"}],
-								$Sstart_fullx, $Send_fullx, $strand, $db, 1);				
-				
-				# making extensions lower case #
-				$proto_seq_full_x = ext_lower_case($proto_seq_full_x,
-										$Sstart_fullx, $Send_fullx,
-										$Sstart_full, $Send_full);					
-				
-				
-				# extracting the pam regions #
-				my ($fiveP_pam, $threeP_pam) = pam_extract($proto_seq_full_x, \@pam, 
-										$Sstart_fullx, $Send_fullx,
-										$Sstart_full, $Send_full);
-				
-				# adding sequences to blast table #
-				## proto sequence extension start-end flip if hits to minus strand #
-				($Sstart_full, $Send_full) = ($Send_full, $Sstart_full) if $strand eq "minus";		
-				($Sstart_fullx, $Send_fullx) = ($Send_fullx, $Sstart_fullx) if $strand eq "minus";	
-				
-
-				# flipping protospacer to blast hit strand #
-				#my $proto_seq_orig = $proto_seq;				# proto sequence relative to query (not potentially revcomp)
-				#if($strand eq "minus"){
-				#	$proto_seq = revcomp($proto_seq);
-				#	($Sstart_full, $Send_full) = ($Send_full, $Sstart_full);
-				#	$proto_seq_full = revcomp($proto_seq_full);
-				#	($Sstart_fullx, $Send_fullx) = ($Send_fullx, $Sstart_fullx);
-				#	$proto_seq_full_x = revcomp($proto_seq_full_x);
-				#	$fiveP_pam = revcomp($fiveP_pam);
-				#	$threeP_pam = revcomp($threeP_pam);
-				#	($fiveP_pam,$threeP_pam) = ($threeP_pam, $fiveP_pam);
-				#	}
-				
-				# adding to table #
-				push @$row, $proto_seq, $Qstart_full, $Qend_full,
-						$Sstart_full, $Send_full, $proto_seq_full,
-						$Sstart_fullx, $Send_fullx, $proto_seq_full_x,
-						$extend, $fiveP_pam, $threeP_pam; 
-				}
-			}
-		}
-	
-		# subject_seq = aln to spacer; subject_full_seq = aln to spacer; subject_full_x_seq = revcomp (for PAMs)
-		#print Dumper %$blast_r; exit;
-	my @new_fields = ("proto_seq", 
-						"q_start_full", "q_end_full", "s_start_full", "s_end_full", "proto_seq_full", 
-						"s_start_fullx", "s_end_fullx", "proto_seq_fullx", "x_length", "5p_pam_region", "3p_pam_region");
-	
-	# status #
-	map{$filter_sum{$_} = 0 unless exists $filter_sum{$_}} qw/total gaps length/;
-	print STDERR "\n### filtering summary ###\n";
-	print STDERR "Total number of hits:\t\t\t\t$filter_sum{'total'}\n";
-	print STDERR "Number removed due to gaps in alignment: \t$filter_sum{'gaps'}\n";
-	print STDERR "Number removed due to short hit length:\t\t$filter_sum{'length'}\n";
-	print STDERR "Number remaining:\t\t\t\t", $filter_sum{'total'} - 
-						($filter_sum{'length'} + $filter_sum{'gaps'}), "\n\n";
-	
-	return \@new_fields;
-	}
-	
-sub pam_extract{
-	my ($proto_seq_full_x, $pam_r, $Sstart_fullx, $Send_fullx, 
-		$Sstart_full, $Send_full) = @_;
-	
-	# getting extension lengths #
-	my $start_x_len = $Sstart_full - $Sstart_fullx;
-	my $end_x_len = $Send_fullx - $Send_full;			
-	my $proto_len = $Send_full - $Sstart_full + 1; 		# inclusive 
-	
-	## 5' end ##
-	my $FiveP_pam_len = abs($$pam_r[0]) - abs($$pam_r[1]) + 1;
-	die " LOGIC ERROR: 5' pam length is < 1!\n" unless $FiveP_pam_len > 0;
-
-	my $FiveP_start = $start_x_len + $$pam_r[0];
-	my $FiveP_pam;
-	if($FiveP_start < 0){		# not enough extension, returning ""
-		$FiveP_pam = "";
-		}
-	else{
-		$FiveP_pam = substr $proto_seq_full_x, $FiveP_start, $FiveP_pam_len;
-		}	
-
-
-	## 3' end #
-	my $ThreeP_pam_len = abs($$pam_r[3]) - abs($$pam_r[2]) + 1;
-	die " LOGIC ERROR: 3' pam length is < 1!\n" unless $ThreeP_pam_len > 0;
-
-	my $ThreeP_start = $proto_len + $start_x_len + $$pam_r[2] -1;
-	my $ThreeP_pam;
-	if($ThreeP_start < 0){		# not enough extension, returning ""
-		$ThreeP_pam = "";
-		}
-	else{
-		$ThreeP_pam = substr $proto_seq_full_x, $ThreeP_start, $ThreeP_pam_len;
-		}
-		#print Dumper $proto_len, $ThreeP_start, $ThreeP_pam_len, $ThreeP_pam;
-	
-		#print Dumper $FiveP_pam, $ThreeP_pam; exit;
-	return $FiveP_pam, $ThreeP_pam; 
-	}
-
-sub ext_lower_case{
-# making extensions to protospacer lower case #
-## substr extensions, and full length; lower case extensions; stitching together ##
-	my ($proto_seq_full_x, $Sstart_fullx, $Send_fullx, 
-		$Sstart_full, $Send_full) = @_;
-	
-	# getting extension lengths #
-	my $start_x_len = $Sstart_full - $Sstart_fullx;
-	my $end_x_len = $Send_fullx - $Send_full;			
-	my $proto_len = $Send_full - $Sstart_full + 1; 		# inclusive 
-	
-	# substringing extensions and full #
-	my $startx_seq = substr($proto_seq_full_x, 0, $start_x_len);
-	my $proto_seq = substr($proto_seq_full_x, $start_x_len, $proto_len); 
-	my $endx_seq = substr($proto_seq_full_x, $start_x_len + $proto_len, $end_x_len);
-
-	# lower case extensions #
-	$startx_seq =~ tr/[A-Z]/[a-z]/;
-	$endx_seq =~ tr/[A-Z]/[a-z]/;
-	
-
-		#print Dumper $startx_seq, $proto_seq, $endx_seq; 
-	return join("", $startx_seq, $proto_seq, $endx_seq);
-	}
-
-sub extend_beyond_full{
-# extending beyond full length #
-	my ($Sstart, $Send, $strand, $Slen, $extend) = @_;
-	
-	$Sstart -= $extend;
-	$Sstart = 1 if $Sstart < 1;
-	$Send += $extend;
-	$Send = $Slen if $Send > $Slen;
-			
-	return $Sstart, $Send;
-	}
-
-sub call_blastdbcmd{
-	my ($subject_id, $Sstart, $Send, $strand, $db) = @_;
-	
-	#($Sstart, $Send) = ($Send, $Sstart) if $strand eq 'minus';
-	
-	# calling #
-	my $cmd = "blastdbcmd -db $db -entry '$subject_id' -range '$Sstart-$Send' -strand $strand |";
-		#print Dumper $cmd; exit;
-	open PIPE, $cmd or die $!;
-	my $seq;
-	while(<PIPE>){
-		chomp;
-		die " ERROR: >1 sequence returned by blastdbcmd!\n"
-			if /^>/ && $seq;
-		next if /^>/;
-		$seq .= $_;
-		}
-		
-		#print Dumper $seq; exit;
-	return $seq;
-	}
-
-sub extend_full_len{
-# extending spacer hit to full length if needed #
-	my ($Sstart, $Send, $strand, $Qstart, $Qend, $Qlen, $Slen) = @_;
-	
-	# missing lengths in spacer blast (if partial hit) #
-	my $Q5px_len = $Qstart - 1;			# extension off 5' end of spacer (+ strand); number of bp added 
-	my $Q3px_len = $Qlen - $Qend;		# extension off 3' end of spacer (+ strand); number of bp added
-	($Q5px_len, $Q3px_len) = ($Q3px_len, $Q5px_len) if $strand eq "minus"; 	# applyin the extension for other strand
-	
-	# changing subject start-end to full length (as much as allowed) #
-	my ($Sstart_full, $Send_full);
-	$Sstart_full = $Sstart - $Q5px_len;
-	$Sstart_full = 1 if $Sstart_full < 1;			# border issues
-	$Send_full = $Send + $Q3px_len; 
-	$Send_full = $Slen if $Send_full > $Slen;		# border issues
-	
-	# getting actual subject extension (acounting for border issues) #
-	my $S5px_len = $Sstart - $Sstart_full;		# number of bp added
-	my $S3px_len = $Send_full - $Send;			# number of bp added
-
-	# making gaps based on difference between intended extend and actual extend #
-	#my $5px_gap = "-" x ($Q5px_len - $S5px_len);
-	#my $3px_gap = "-" x ($Q3px_len - $S3px_len);
-	#map{$_ = "" unless $_} ($5px_gap, $3px_gap);
-	#($5px_gap, $3px_gap) = ($3px_gap, $5px_gap) if $strand eq "minus"; 		# gap applied to the other end
-	
-	# getting qstart/qend full (as must as subject extension would allow) #	
-	($S5px_len, $S3px_len) = ($S3px_len, $S5px_len) if $strand eq "minus"; 	# applying the extension for other strand
-	my $Qstart_full = $Qstart - $S5px_len;		
-	my $Qend_full = $Qend + $S3px_len;
-			
-	# sanity check #
-	die " LOGIC ERROR $!" unless $Qstart_full > 0 && $Qend_full > 0;
-	die " LOGIC ERROR $!" unless $Sstart_full > 0 && $Send_full > 0;
-	
-	return $Sstart_full, $Send_full, $Qstart_full, $Qend_full; #$5px_gap, $3px_gap;
-	}
-	
-sub check_values{
-# checking values to make sure they exist #
-	my ($row, $fields_r) = @_;
-	
-	#print Dumper $row;
-	#print Dumper $fields_r; 
-	
-	my @chk = ("query id", "subject id", "s. start", "s. end",
-				"q. start", "q. end", 
-				"evalue", "query length", "subject length", "BTOP");
-	map{die " ERROR: '$_' not found!\n" unless $$row[$fields_r->{$_}]} @chk;
-	}
-
 sub load_fasta{
-# loading fasta file as a hash #
-	my $fasta_in = shift;
-	open IN, $fasta_in or die $!;
-	my (%fasta, $tmpkey);
-	while(<IN>){
-		chomp;
- 		s/#.+//;
- 		next if  /^\s*$/;	
- 		if(/>.+/){
- 			$_ =~ s/^>//;
- 			$fasta{$_} = "";
- 			$tmpkey = $_;	# changing key
- 			}
- 		else{$fasta{$tmpkey} .= $_; }
-		}
-	close IN;
-		#print Dumper %fasta; exit;
-	return \%fasta;
-	} #end load_fasta
-
+  # loading fasta file as a hash #
+  my $fasta_in = shift;
+  open IN, $fasta_in or die $!;
+  my (%fasta, $tmpkey);
+  while(<IN>){
+    chomp;
+    s/#.+//;
+    next if  /^\s*$/;	
+    if(/>.+/){
+      $_ =~ s/^>//;
+      $fasta{$_} = "";
+      $tmpkey = $_;	# changing key
+    }
+    else{$fasta{$tmpkey} .= $_; }
+  }
+  close IN;
+  #print Dumper %fasta; exit;
+  return \%fasta;
+} #end load_fasta
 
 
